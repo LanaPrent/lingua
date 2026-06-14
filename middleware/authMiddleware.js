e1dedc9e28ac43faa58e8dad64d1b65a7a6ba22b
Lana <sp.englishlang@yahoo.com>
2026-06-15 01:37:33 +0200

    authMiddleware.js, redirect instead of new page with error message

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,11 +4,12 @@ function isAuthenticated(req, res, next) {
     if (req.session.userId) {
         return next();
     }
-
+/*
     return res.status(401).json({
         message: "Unauthorized"
     });
-    
+*/    
+return res.redirect("/login.html");
 }
 
 module.exports = isAuthenticated;
