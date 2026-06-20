afef09803c7458c23ad309d4aa9c01b0f0bf2ac5
Lana <sp.englishlang@yahoo.com>
2026-06-20 14:40:10 +0200

    contactRouter.js, added code

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,14 +3,23 @@ const router = express.Router();
 
 const csrfProtection = require("../middleware/csrf");
 
-const badWordFilter = require("../middleware/badWordFilter");
-
-const {
-    submitContactForm
-} = require("../controllers/contactController");
-
+// Middleware (all safely imported)
 const honeypotGuard = require("../middleware/honeypotGuard");
+const badWordFilter = require("../middleware/badWordFilter");
+const adminBlockIP = require("../middleware/blackListIP");
 
+// Controller
+const { submitContactForm } = require("../controllers/contactController");
+
+/**
+ * CONTACT FORM ROUTE
+ * Order matters:
+ * 1. IP block (fast reject)
+ * 2. CSRF validation
+ * 3. honeypot check
+ * 4. content filter
+ * 5. actual controller
+ */
 
 router.post(
   "/submit",
@@ -20,8 +29,6 @@ router.post(
   badWordFilter,
   submitContactForm
 );
-
-
 
 module.exports = router;
 
