6b20b8335d63a302f53841b47849e2c560b507a5
Lana <sp.englishlang@yahoo.com>
2026-06-20 05:23:50 +0200

    honeypotGuard.js function modified

--- middleware/honeypotGuard.js
+++ middleware/honeypotGuard.js
@@ -1,12 +1,15 @@
 function honeypotGuard(req, res, next) {
-  const { website, formStartTime } = req.body;
+  const body = req.body || {};
+  const { website, formStartTime } = body;
 
-  // 1. Honeypot field check
+// 1. Honeypot field check
+
   if (website && website.length > 0) {
     return res.status(400).send("Bot detected");
   }
+// 2. Time check (minimum 2 seconds)
 
-  // 2. Time check (minimum 2 seconds)
+
   if (formStartTime) {
     const duration = Date.now() - Number(formStartTime);
 
@@ -17,5 +20,6 @@ function honeypotGuard(req, res, next) {
 
   next();
 }
+
 
 module.exports = honeypotGuard;
